804c82d14ef54c0cd642c21cc3a22a5990c12aee
ConnorMCode <ConnorMontenegro00@gmail.com>
2025-03-03 22:53:34 +0000

    base implementation, only exit syscall added

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -4,6 +4,7 @@
 #include "devices/block.h"
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "userprog/pagedir.h"
 
 static void syscall_handler (struct intr_frame *);
 
@@ -14,6 +15,24 @@ void syscall_init (void)
 
 static void syscall_handler (struct intr_frame *f UNUSED)
 {
-  printf ("system call!\n");
-  thread_exit ();
+  if (!is_valid_user_pointer(f->esp)){
+    exit(-1);
+  }
+
+  int *ptr = (int *)f->esp
+
+  int syscall_number = *ptr;
+
+  switch (syscall_number) {
+  case SYS_EXIT:
+    if(pointer_validate
+    exit(*(ptr+1));
+    break;
+  }
 }
+
+static bool pointer_validate(const void *ptr) {
+  bool validate = (ptr != NULL && is_user_vaddr(ptr) && pagedir_get_page(thread_current()->pagedir, ptr) != NULL);
+  return validate;
+}
+
